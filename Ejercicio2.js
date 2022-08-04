b7aedb482ff26991a23d1bcaa745c67355bf6082
Brandon Crespo <brandon-18@hotmail.es>
2022-08-04 23:07:22 +0200

    Se crea script de Ejercicio2.js

---
+++ Ejercicio2.js
@@ -0,0 +1,10 @@
+const info = [ "Brandon",
+                 24,
+                 true,
+                 new Date(1997, 10, 21),
+                 libro = { 
+                    titulo: 'Aprende JS',
+                    anyo: 1990} 
+            ]
+
+console.log(info)
