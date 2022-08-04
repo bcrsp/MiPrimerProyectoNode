94ae0d1b910530241f4f5af590e76c28d4f89732
Brandon Crespo <brandon-18@hotmail.es>
2022-08-04 22:45:38 +0200

    Se crea nuevo script notaciones.js

---
+++ notaciones.js
@@ -0,0 +1,29 @@
+//Notacion
+// ; -> Delimitar el final de una linea. A día de hoy ya se puede omitir
+const b = 4;
+
+// . -> Se utiliza en los objetos para acceder a los atributos
+const movil = {
+    anchura:10
+}
+console.log(movil.anchura);
+
+// [] -> square braquets  -> Se utilizan para las listas, arreglos o arrays
+const arr = [1,2,3,4];
+console.log(arr[2]);
+
+// () -> Funciones
+
+const nuevaFuncion = () => {
+    console.log("Dentro de una funcion");
+}
+
+// {} -> Llaves para objetos, funciones y estructura d control
+
+const telefono = {
+    anchura:10
+}
+
+if(true){
+
+}
