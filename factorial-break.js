0cf38732195328065caf6f9954ebab6e29300629
Brandon Crespo <brandon-18@hotmail.es>
2022-08-08 23:38:01 +0200

    Se crea scripts de ejercicios factoria-break, factoreial-for y factorial-while

---
+++ factorial-break.js
@@ -0,0 +1,11 @@
+
+let i  = 1; 
+let factorial = 1;
+while(true){
+    if(i === 11){
+        break;
+    }
+
+    factorial = factorial * i;
+    i++;
+}
---
+++ factorial-while.js
@@ -0,0 +1,7 @@
+let  i = 1;
+let factorial = 1;
+
+while(i < 11){
+    factorial = factorial * i;
+    i++;
+}
